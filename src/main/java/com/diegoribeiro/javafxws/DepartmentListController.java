a114fc73804961b147574a072254593cf39c86bc
Diego <eliwandejp@gmail.com>
2025-11-22 12:09:50 -0300

    DepartmentForm (dialog) design

--- src/main/java/com/diegoribeiro/javafxws/DepartmentListController.java
+++ src/main/java/com/diegoribeiro/javafxws/DepartmentListController.java
@@ -2,16 +2,25 @@ package com.diegoribeiro.javafxws;
 
 import com.diegoribeiro.javafxws.model.entities.Department;
 import com.diegoribeiro.javafxws.model.services.DepartmentService;
+import com.diegoribeiro.javafxws.util.Alerts;
+import com.diegoribeiro.javafxws.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -35,8 +44,9 @@ public class DepartmentListController implements Initializable {
     private ObservableList<Department> departmentObservableList;
 
     @FXML
-    public void onBtNewAction() {
-        System.out.println("onBtNewAction");
+    public void onBtNewAction(ActionEvent event) {
+        Stage parentStage = Utils.currentStage(event);
+        createDialogForm("DepartmentForm.fxml", parentStage);
     }
 
     public void setDepartmentService(DepartmentService service) {
@@ -57,12 +67,30 @@ public class DepartmentListController implements Initializable {
     }
 
     public void updateTableView() {
-        if ( service == null) {
+        if (service == null) {
             throw new IllegalStateException("Service is null");
         }
 
         List<Department> list = service.findAll();
         departmentObservableList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(departmentObservableList);
+    }
+
+    private void createDialogForm(String absoluteName, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Department data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
